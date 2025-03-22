e74e45a5c89d58e3be9c9ef715c1023fa34342a8
krnkiran22 <kirandev2210@gmail.com>
2025-03-22 15:39:42 +0530

    add

--- routes/courseRoutes.js
+++ routes/courseRoutes.js
@@ -35,4 +35,16 @@ router.delete('/delete/:id', async (req, res) => {
     }
 });
 
+router.get('/:courseId', async (req, res) => {
+    try {
+      const course = await Course.findOne({ courseId: req.params.courseId });
+      if (!course) {
+        return res.status(404).json({ message: 'Course not found' });
+      }
+      res.json(course);
+    } catch (err) {
+      res.status(500).json({ message: err.message });
+    }
+  });
+  
 module.exports = router;
