5fd0478ff5a8a2a66171bfb9d173ac78e258c93c
krnkiran22 <kirandev2210@gmail.com>
2025-03-22 18:14:41 +0530

    wallet

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const courseRoutes = require('../routes/courseRoutes');
-const walletRoutes = require('./routes/walletRoutes');
+const walletRoutes = require('../routes/walletRoutes');
 const app = express();
 app.use(express.json());
 app.use(cors());
